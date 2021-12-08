d6db1fa2d26d3fda2f8fbea9336bb71fcd891d02
Aaron Brewster <aaron@thelonelypixel.co.uk>
2021-12-08 13:21:05 +0000

    DBLP Bundle

--- assets/js/dblp-bundle.js
+++ assets/js/dblp-bundle.js
@@ -104473,15 +104473,15 @@ function extend() {
 
 },{}]},{},[1]);
 
-function printJS(jsObject) {
-  console.log(JSON.stringify(jsObject, null, 2));
-}
+// function printJS(jsObject) {
+//   console.log(JSON.stringify(jsObject, null, 2));
+// }
 
-const dblp = new DBLP();
+// const dblp = new DBLP();
 
-async function extractInfo() {
-  var tiago_json = await dblp.getByPID('188/5658');
-  printJS(tiago_json.getJSON());
-}
+// async function extractInfo() {
+//   var tiago_json = await dblp.getByPID('188/5658');
+//   printJS(tiago_json.getJSON());
+// }
 
-extractInfo();
+// extractInfo();
